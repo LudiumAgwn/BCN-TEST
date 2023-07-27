f8fce1405ab962ec5804bc65bc0b4b41f62302bf
LudiumAgwn <agwn@ludium.community>
2023-07-28 08:11:09 +0900

    W2 복사

---
+++ W2/ft 복사본/target/wasm32-unknown-unknown/release/fungible_token.d
@@ -0,0 +1,1 @@
+/Users/abaret/Desktop/개발\ 교육\ 프로그램/BCN-Mission2/ft/target/wasm32-unknown-unknown/release/fungible_token.wasm: /Users/abaret/Desktop/개발\ 교육\ 프로그램/BCN-Mission2/ft/src/lib.rs
---
+++ W2/ft 복사본/target/wasm32-unknown-unknown/release/libfungible_token.d
@@ -0,0 +1,1 @@
+/Users/abaret/Desktop/개발\ 교육\ 프로그램/BCN-Mission2/ft/target/wasm32-unknown-unknown/release/libfungible_token.rlib: /Users/abaret/Desktop/개발\ 교육\ 프로그램/BCN-Mission2/ft/src/lib.rs
